6f2b7cc94a3db23e52bbc9ea2263cfb81d5266e1
zhouzhixin98 <343766265@qq.com>
2022-10-20 14:24:43 +0800

    test commit

---
+++ src/com/jiuGHim/singleTon/test.java
@@ -0,0 +1,13 @@
+package com.jiuGHim.singleTon;
+
+/**
+ * description: test
+ * date: 2022/10/20 14:24:15
+ *
+ * @author: zzx
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
