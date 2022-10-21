5ab93b78b78d7255608b31e6106e9087d6ce8144
zhouzhixin98 <343766265@qq.com>
2022-10-21 09:16:59 +0800

    单例模式demo

---
+++ src/com/jiuGHim/singleTon/SingletonDemoInDoubleCheckLock2.java
@@ -0,0 +1,39 @@
+package com.jiuGHim.singleTon;
+
+/**
+ * description: SingletonDemoInDoubleCheckLock2
+ * date: 2022/10/20 17:34:24
+ *
+ * @author: zzx
+ */
+
+/**
+ * 双重校验锁单例模式，去除volatile关键字
+ */
+public class SingletonDemoInDoubleCheckLock2 {
+
+    /**
+     * 没有volatile修饰实例
+     */
+    private static SingletonDemoInDoubleCheckLock2 instance = null;
+
+    /**
+     * 私有构造
+     */
+    private SingletonDemoInDoubleCheckLock2() {}
+
+    /**
+     * 公共获取实例方法
+     * @return
+     */
+    public static SingletonDemoInDoubleCheckLock2 getInstance() {
+        if (instance == null){
+            synchronized (SingletonDemoInDoubleCheckLock2.class){
+                if (instance == null){
+                    instance = new SingletonDemoInDoubleCheckLock2();
+                }
+            }
+        }
+        return instance;
+    }
+}
